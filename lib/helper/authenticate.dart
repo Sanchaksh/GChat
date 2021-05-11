bf6928a53c9f1c64e62c14610323005c09399d9b
Sanchaksh <sanchakshkaul@gmail.com>
2021-05-11 13:48:04 +0530

    Backend: Connected the SignIn, SignUp and Chatroom pages with page replacement with a signout functionality.

---
+++ lib/helper/authenticate.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:g_chat/views/signin.dart';
+import 'package:g_chat/views/signup.dart';
+
+class Authenticate extends StatefulWidget {
+  const Authenticate({Key key}) : super(key: key);
+
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+
+  bool showSignIn = true;
+
+  void toggleView(){
+    setState(() {
+      showSignIn = !showSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(showSignIn){
+      return SignIn(toggleView);
+
+    }else{
+      return SignUp(toggleView);
+   }
+  }
+}
