96c1bda5f97544eee3fed937d1de7e27782023a4
Sanchaksh <sanchakshkaul@gmail.com>
2021-05-16 01:01:39 +0530

    Updated some forgotpassword functionality

--- lib/views/forgetpassword.dart
+++ lib/views/forgetpassword.dart
@@ -1,13 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:g_chat/services/auth.dart';
 
-class ForgotPassword extends StatefulWidget {
+class ResetPass extends StatefulWidget {
   @override
-  _ForgotPasswordState createState() => _ForgotPasswordState();
+  _ResetPassState createState() => _ResetPassState();
 }
 
-class _ForgotPasswordState extends State<ForgotPassword> {
+class _ResetPassState extends State<ResetPass> {
+  String email;
+  final _formKey = GlobalKey<FormState>();
+  final AuthMethods _authService = AuthMethods();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [const Color(0xff213A50), const Color(0xff071930)],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight)),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              padding:
+              EdgeInsets.only(left: 16, right: 16, top: 140, bottom: 140),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Flutter",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "Chat",
+                            style: TextStyle(
+                                color: Color(0xffD9B372),
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      width: MediaQuery.of(context).size.width > 800
+                          ? 400
+                          : MediaQuery.of(context).size.width - 60,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            style: TextStyle(color: Colors.white),
+                            validator: (val) =>
+                            val.isEmpty ? "Enter an Email" : null,
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle: TextStyle(
+                                  color: Colors.white.withOpacity(0.5)),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                            onChanged: (val) {
+                              setState(() {
+                                email = val;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    GestureDetector(
+                      onTap: () async {
+                        if (_formKey.currentState.validate()) {
+                          await _authService.resetPass(email);
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width > 800
+                            ? 400
+                            : MediaQuery.of(context).size.width - 60,
+                        padding:
+                        EdgeInsets.symmetric(vertical: 19, horizontal: 30),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(30))),
+
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
