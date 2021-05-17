b92f8b61e3be10a548b2aae9ee16b8eda63abbc5
Sanchaksh <sanchakshkaul@gmail.com>
2021-05-18 00:11:08 +0530

    Updated getMessages to Future

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -3,35 +3,37 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseMethods {
   getUsersbyUsername(String username) async {
    return await FirebaseFirestore.instance.collection("users").where("name", isEqualTo: username).get();
-
-  }
-  getUsersbyUserEmail(String userEmail) async {
-    return await FirebaseFirestore.instance.collection("users").where("email", isEqualTo: userEmail).get();
   }
 
-  uploadUserInfo(userMap) {
+  getUsersbyUserEmail(String email) async {
+    return await FirebaseFirestore.instance.collection("users").where("email", isEqualTo: email).get().catchError((e){
+      print(e.toString());
+    });
+  }
+
+ Future<void> uploadUserInfo(userMap) {
     FirebaseFirestore.instance.collection("users").add(userMap).catchError((e){
       print(e.toString());
     });
   }
+
   Future <void> createChatRoom(chatroomId, chatRoomMap) {
     FirebaseFirestore.instance.collection("chatRoom").doc(chatroomId).set(chatRoomMap).catchError((e){
-      print(e.toString());
+      print(e);
     });
   }
 
-  Future <void> addConversionMessage(String chatRoomId, messageMap) {
-    FirebaseFirestore.instance.collection("chatRoom").doc(chatRoomId).collection("chats").add(messageMap).catchError((e) {
+  Future <void> addConversionMessage(String chatroomId, messageMap) {
+    FirebaseFirestore.instance.collection("chatRoom").doc(chatroomId).collection("chats").add(messageMap).catchError((e) {
       print(e.toString());
     });
   }
 
   getConversionMessage(String chatroomId) async {
-    return await FirebaseFirestore.instance.collection("chatRoom").doc(chatroomId).collection("chats").orderBy("time").snapshots();
+    return FirebaseFirestore.instance.collection("chatRoom").doc(chatroomId).collection("chats").orderBy("time").snapshots();
   }
 
   getChatRooms(String userName) async {
     return await FirebaseFirestore.instance.collection("chatRoom").where("users", arrayContains: userName).snapshots();
   }
-
 }
